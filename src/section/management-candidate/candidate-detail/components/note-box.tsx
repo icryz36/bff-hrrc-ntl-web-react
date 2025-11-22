8d1dbbd1cd7fa71be2522e966b1e0023e2d3a981
Kiattisak Sonthong <ext.kiattisak.son@truedigital.com>
2025-11-22 20:56:05 +0700

    add text

--- src/section/management-candidate/candidate-detail/components/note-box.tsx
+++ src/section/management-candidate/candidate-detail/components/note-box.tsx
@@ -1,7 +1,18 @@
-import { TextField } from '@mui/material';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
 const NoteBox = () => {
-  return <TextField label="write a Note" multiline rows={4} fullWidth />;
+  return (
+    <Box>
+      <TextField label="write a Note" multiline rows={4} fullWidth />
+      <Stack pt={1} position={'absolute'} right={0} gap={'4px'}>
+        <Typography variant="caption_semibold">100</Typography>
+        <Typography variant="caption_regular">/ 255</Typography>
+      </Stack>
+      <Button size="medium" variant="contained" sx={{ mt: 3 }}>
+        Add note
+      </Button>
+    </Box>
+  );
 };
 
 export default NoteBox;
